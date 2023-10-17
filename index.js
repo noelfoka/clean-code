44f8a831a8581ccd6b6369232759715c41544145
unknown <nomgnenoel@gmail.com>
2023-10-17 21:47:24 +0100

    significan name

---
+++ index.js
@@ -0,0 +1,17 @@
+// significan name
+//Bad
+const customerList = [▬];
+const theTable = (customers) => {
+    return theTable;
+}
+
+theTable(customerList);
+
+// Good
+
+const customers = [];
+const customerTable = (customers) => {
+    return theTable;
+}
+
+customerTable(customers);
